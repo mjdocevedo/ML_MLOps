736b4aa6e72f62ffa74f64c535b63c0ab98941d1
MJ Domenzain <47764531+mjdocevedo@users.noreply.github.com>
2024-06-13 17:07:50 +0200

    Data validation added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from src.ML_MLOps import logger
 from ML_MLOps.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ML_MLOps.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\n x======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataValidationTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\n x======x")
 except Exception as e:
